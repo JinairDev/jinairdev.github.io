377e229c2e91314652f2a072dbff3701f17681c4
Jiho Kim <go2zo.ooo@gmail.com>
2022-05-27 16:48:14 +0900

    chore: add deploy script for gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  target: "static",
   css: [
     '~/assets/styles/quasar.sass',
     '@quasar/extras/roboto-font/roboto-font.css',
