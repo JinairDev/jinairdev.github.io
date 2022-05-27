e06e75bada88621d25faffa8a4a4590a82cf0b08
Jiho Kim <go2zo.ooo@gmail.com>
2022-05-27 17:33:37 +0900

    feat: add layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <client-only>
+    <q-layout view="hHh Lpr fFf">
+      <q-header elevated class="glossy">
+        <q-toolbar>
+          <q-btn
+            class="lt-sm"
+            flat
+            dense
+            round
+            aria-label="Menu"
+            icon="menu"
+            @click="leftDrawerOpen = !leftDrawerOpen" />
+        </q-toolbar>
+      </q-header>
+    </q-layout>
+  </client-only>
+</template>
+
+<script setup lang="ts">
+const leftDrawerOpen = ref(false);
+</script>
